dbb73203d4fb7529f7047fbc856a956544c98066
Caleb Kiragu <calebdavidbusiness@gmail.com>
2026-05-10 02:41:24 +0300

    fix: switch MemoryRouter to BrowserRouter so direct /pay/:id links work

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ window.Buffer = Buffer;
 
 import './index.css';
 import { createRoot } from 'react-dom/client';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
-  <MemoryRouter>
+  <BrowserRouter>
     <App />
-  </MemoryRouter>
+  </BrowserRouter>
 );
